b4e1211c41b300a26d2ec2ccab7acb586127c15d
MatGonPer <matheushtht@gmail.com>
2025-09-20 20:30:24 -0300

    Criando estrutura inicial do projeto

---
+++ build/CMakeFiles/NostromoOS.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NostromoOS.dir/link.d"
+  "CMakeFiles/NostromoOS.dir/src/main.cpp.o"
+  "CMakeFiles/NostromoOS.dir/src/main.cpp.o.d"
+  "NostromoOS"
+  "NostromoOS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NostromoOS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/NostromoOS.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for NostromoOS.
+# This may be replaced when dependencies are built.
